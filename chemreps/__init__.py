fe2cc73b3bf8da4cf67273f651e674fd0c38eca5
amandadumi <amanda.e.dumi@gmail.com>
2020-03-26 13:12:31 -0400

    including updates rdkit check, import warning, ignoring code for tests in case of no rdkit

--- chemreps/__init__.py
+++ chemreps/__init__.py
@@ -9,6 +9,8 @@ from . import bag_of_bonds
 from . import bat
 from . import just_bonds
 from . import dataset
-rdkit_loader = importlib.find_loader('rdkit')
+rdkit_loader = importlib.util.find_spec('rdkit')
 if rdkit_loader is not None:
     from . import fingerprints
+elif rdkit_loader is None: 
+    raise ImportWarning('RDKit was not found. As a result, the Morgan fingerprint representation module has not been imported.')
