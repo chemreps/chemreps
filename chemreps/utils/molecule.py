b44b6edcebea53504f60809dcce856d29a437c57
Ryoo <Triry182@hotmail.com>
2020-03-27 09:31:34 +0900

    Update molecule.py

--- chemreps/utils/molecule.py
+++ chemreps/utils/molecule.py
@@ -409,26 +409,3 @@ class Molecule:
                 if self.bond(i, j):
                     temp.append([i+1, j+1])
         self.connect = np.asarray(temp)
-
-
-def length(molecule, atomi, atomj):
-    """
-    Returns the length between two atoms
-
-    Parameters
-    -----------
-    molecule : object
-        molecule object
-    atomi, atomj : int
-        atoms
-
-    Returns
-    --------
-    rij : float
-        length between the two
-    """
-    x = molecule.xyz[atomi][0] - molecule.xyz[atomj][0]
-    y = molecule.xyz[atomi][1] - molecule.xyz[atomj][1]
-    z = molecule.xyz[atomi][2] - molecule.xyz[atomj][2]
-    rij = sqrt((x ** 2) + (y ** 2) + (z ** 2))
-    return np.float16(rij)
