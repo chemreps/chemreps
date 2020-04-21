6d7b81463e1d0f2cd35fa49a9fe018b5205f8425
Shiv Upadhyay <shivnupadhyay@gmail.com>
2020-04-20 23:30:02 -0400

    Adding a test

--- tests/optional_dependency_test.py
+++ tests/optional_dependency_test.py
@@ -1,22 +1,34 @@
 from unittest import mock
 import pytest
 import warnings
+import builtins
+import importlib
 
+@pytest.fixture
+def hide_available_rdkit(monkeypatch):
+    import_orig = builtins.__import__
+    def mocked_import(name, *args, **kwargs):
+        if name == 'rdkit':
+            raise ImportError()
+        return import_orig(name, *args, **kwargs)
+    monkeypatch.setattr(builtins, '__import__', mocked_import)
 
+@pytest.mark.usefixtures('hide_available_rdkit')
 def test_warning_raised_if_rdkit_not_installed():
     #https://stackoverflow.com/questions/60746184/test-behavior-of-code-if-optional-module-is-not-installed
-    import sys
+    #import sys
 
     # clear cached modules
-    mods = list(sys.modules.keys())
-    for mod in mods:
-        if 'chemreps' in mod or 'rdkit' in mod:
-            del sys.modules[mod]
-
-    sys.modules['rdkit'] = mock.MagicMock()
-    with warnings.catch_warnings(record=True) as w:
+    #mods = list(sys.modules.keys())
+    #if 'rdkit' in mods:
+    #     del sys.modules['rdkit']
+    #sys.modules['rdkit'] = mock.MagicMock()
+    #with warnings.catch_warnings(record=True) as w:
+    with pytest.warns(UserWarning):
         import chemreps
-        assert len(w) > 0
-        assert issubclass(w[-1].category, UserWarning)
-        assert "RDKit was not found. As a result, the Morgan fingerprint representation module has not been imported." == str(w[-1].message)
-    del sys.modules['rdkit']
+        importlib.reload(chemreps)
+        #print(w)
+        #assert len(w) > 0
+        #assert issubclass(w[-1].category, UserWarning)
+        #assert "RDKit was not found. As a result, the Morgan fingerprint representation module has not been imported." == str(w[-1].message)
+    #del sys.modules['rdkit']
