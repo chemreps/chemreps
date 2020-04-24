716887463258841f9ad5a60807b20ec4df9a5a96
Shiv Upadhyay <shivnupadhyay@gmail.com>
2020-04-23 21:46:22 -0400

    Working test

--- tests/optional_dependency_test.py
+++ tests/optional_dependency_test.py
@@ -3,17 +3,27 @@ import pytest
 import warnings
 import builtins
 import importlib
+import sys
 
-@pytest.fixture
-def hide_available_rdkit(monkeypatch):
-    import_orig = builtins.__import__
-    def mocked_import(name, *args, **kwargs):
-        if name == 'rdkit':
-            raise ImportError()
-        return import_orig(name, *args, **kwargs)
-    monkeypatch.setattr(builtins, '__import__', mocked_import)
+#@pytest.fixture
+#def hide_available_rdkit(monkeypatch):
+#    import_orig = builtins.__import__
+#    def mocked_import(name, *args, **kwargs):
+#        if name == 'rdkit':
+#            raise ImportError()
+#        return import_orig(name, *args, **kwargs)
+#    
+#
+#    def mocked_spec(name, *args, **kwargs):
+#        print(name)
+#        if name == 'rdkit':
+#            return None
+#        return sys.modules[name].__spec__
+#
+#    monkeypatch.setattr(builtins, '__import__', mocked_import)
+#    monkeypatch.setattr(builtins, '__spec__', mocked_spec)
 
-@pytest.mark.usefixtures('hide_available_rdkit')
+#@pytest.mark.usefixtures('hide_available_rdkit')
 def test_warning_raised_if_rdkit_not_installed():
     #https://stackoverflow.com/questions/60746184/test-behavior-of-code-if-optional-module-is-not-installed
     #import sys
@@ -22,9 +32,11 @@ def test_warning_raised_if_rdkit_not_installed():
     #mods = list(sys.modules.keys())
     #if 'rdkit' in mods:
     #     del sys.modules['rdkit']
-    #sys.modules['rdkit'] = mock.MagicMock()
     #with warnings.catch_warnings(record=True) as w:
     with pytest.warns(UserWarning):
+        patcher = mock.patch('importlib.util.find_spec')
+        importlib_find_spec_mock = patcher.start()
+        importlib_find_spec_mock.return_value = None
         import chemreps
         importlib.reload(chemreps)
         #print(w)
